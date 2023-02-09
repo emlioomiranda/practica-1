4d3bf5d044959919536767fa421bba525cb2ab68
jorge emilio <emilioo.antunezz@gmail.com>
2023-02-09 07:51:55 -0600

    sumaemilio miranda

---
+++ src/sumaemilio.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class sumaemilio {
+    public static void sumaemilio String[] args){
+    Scanner objets= new Scanner (System.in);
+    int suma, n1,n2:
+        System.out.println("da el primer numero");
+    n1=objet.nextInt();
+    System.out.println("da el segundo numero");
+    n2=objet.nextInt();
+
+    suma=n1+n2;
+    System.out.println("la suma es + suma");
+
+    }
+}
